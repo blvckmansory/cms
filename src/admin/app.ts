32b2a239ca75c9bdb943204585e4179fa7a29a78
zkharov <zakharovvadzim@yandex.by>
2023-12-18 21:41:15 +0300

    :recycle: FEAUTURE brand translations

--- src/admin/app.ts
+++ src/admin/app.ts
@@ -1,38 +1,46 @@
 import ru from '../../src/admin/src/translations/ru.json'
 
+const brandTranslations = {
+	Car: 'Машина',
+	icon: 'Иконка',
+	label: 'Текст',
+	cover: 'Обложка',
+	title: 'Заголовок',
+	content: 'Содержание',
+	description: 'Описание',
+	carName: 'Модель машины',
+
+	'HomePage.helmet.title': 'Домашняя страница',
+	'Content Manager': 'Редактор',
+	'Content Type Builder': 'Конструктор типов',
+	'content-type-builder.plugin.name': 'Конструктор типов',
+	'react-icons.plugin.name': 'Библиотека иконок',
+	'import-export-entries.plugin.name': 'Импорт & Экспорт',
+	'import-export-entries.plugin.description':
+		'Позволяет импортировать и экспортировать данные в бд.',
+	'notification.error.revalidate': 'Ошибка ревалидации',
+	'multi-select.plugin.name': 'Мульти-Селект',
+	'admin.pages.MarketPlacePage.helmet': 'Библиотека плагинов',
+	'app.components.HomePage.welcome': 'Добро пожаловать!',
+	'app.components.HomePage.welcome.again': 'И снова здравствуй!',
+	'plugin.schema.i18n.localized.label-content-type': 'i18n',
+	'plugin.schema.i18n.localized.label-field-type': 'i18n',
+	'Auth.form.welcome.title': 'Админ панель хеллошки!',
+	'Auth.form.welcome.subtitle': 'Войдите и настроивайте!',
+	'app.components.LeftMenu.navbrand.title': 'Hello.by',
+}
+
 export default {
 	config: {
 		locales: ['ru', 'en'],
-
 		translations: {
+			en: {
+				...ru,
+				...brandTranslations,
+			},
 			ru: {
 				...ru,
-				Car: 'Машина',
-				icon: 'Иконка',
-				label: 'Текст',
-				cover: 'Обложка',
-				title: 'Заголовок',
-				content: 'Содержание',
-				description: 'Описание',
-				carName: 'Модель машины',
-
-				'HomePage.helmet.title': 'Домашняя страница',
-				'Content Manager': 'Редактор',
-				'Content Type Builder': 'Конструктор типов',
-				'content-type-builder.plugin.name': 'Конструктор типов',
-				'react-icons.plugin.name': 'Библиотека иконок',
-				'import-export-entries.plugin.name': 'Импорт & Экспорт',
-				'import-export-entries.plugin.description':
-					'Позволяет импортировать и экспортировать данные в бд.',
-				'notification.error.revalidate': 'Ошибка ревалидации',
-				'multi-select.plugin.name': 'Мульти-Селект',
-				'admin.pages.MarketPlacePage.helmet': 'Библиотека плагинов',
-				'app.components.HomePage.welcome': 'Добро пожаловать!',
-				'app.components.HomePage.welcome.again': 'И снова здравствуй!',
-				'plugin.schema.i18n.localized.label-content-type': 'i18n',
-				'plugin.schema.i18n.localized.label-field-type': 'i18n',
-				'Auth.form.welcome.title': 'Админ панель хеллошки!',
-				'Auth.form.welcome.subtitle': 'Войдите и настроивайте!',
+				...brandTranslations,
 			},
 		},
 		tutorials: false,
