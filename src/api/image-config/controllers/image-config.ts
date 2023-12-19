6631d2ca35e94b6730eb0a58ea2da5872b0b50c8
zkharov <zakharovvadzim@yandex.by>
2023-12-20 00:47:02 +0300

    :boom: ADD SingleTypes

---
+++ src/api/image-config/controllers/image-config.ts
@@ -0,0 +1,7 @@
+/**
+ * image-config controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::image-config.image-config');
---
+++ src/api/image-config/services/image-config.ts
@@ -0,0 +1,7 @@
+/**
+ * image-config service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::image-config.image-config');
