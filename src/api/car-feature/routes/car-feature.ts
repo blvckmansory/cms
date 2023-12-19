d235e9846ff0e4ab45d186b1d934e2cc5f6d9401
zkharov <zakharovvadzim@yandex.by>
2023-12-20 00:44:25 +0300

    :fire: DELETE CarFeature

--- src/api/car-feature/routes/car-feature.ts
+++
@@ -1,7 +0,0 @@
-/**
- * car-feature router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::car-feature.car-feature');
--- src/api/car-feature/services/car-feature.ts
+++
@@ -1,7 +0,0 @@
-/**
- * car-feature service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::car-feature.car-feature');
