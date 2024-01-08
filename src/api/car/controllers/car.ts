6386063b79242d95862b22d79da27a87b05ab58c
zkharov <zakharovvadzim@yandex.by>
2024-01-08 18:58:43 +0300

    :ambulance: UPDATE controllers

--- src/api/car/controllers/car.ts
+++ src/api/car/controllers/car.ts
@@ -116,7 +116,9 @@ export default factories.createCoreController(uid, ({ strapi }) => ({
 						fields: ['color'],
 						populate: { image: { fields: ['url'] } },
 					},
-
+					conditions: {
+						fields: ['id', 'name'],
+					},
 					rates: {
 						fields: ['id'],
 						populate: {
