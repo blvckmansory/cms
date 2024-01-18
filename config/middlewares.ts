f59bd4eb7e4723bc803f8bcae9ea04d61b3aef5a
mtsimafeyeu <iambadfan@gmail.com>
2024-01-18 22:11:42 +0300

    ♻️ change media library config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,17 @@ export default [
 	'strapi::cors',
 	'strapi::logger',
 	'strapi::query',
-	'strapi::body',
+	{
+		name: "strapi::body",
+		config: {
+			formLimit: "100mb", // modify form body
+			jsonLimit: "100mb", // modify JSON body
+			textLimit: "100mb", // modify text body
+			formidable: {
+				maxFileSize: 100 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+			},
+		},
+	},
 	'strapi::session',
 	'strapi::favicon',
 	'strapi::public',
